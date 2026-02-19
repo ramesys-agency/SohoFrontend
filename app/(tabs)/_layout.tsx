f1ab64a52eff78c6b9f9616d736598f02abdc2dc
arshad-khan-pinn <arshad.khan@pinnacle.in>
2026-02-19 16:01:54 +0530

    fix: navbar not shown in category page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -119,6 +119,14 @@ export default function TabsLayout() {
           tabBarLabel: () => null,
         }}
       />
+
+      {/* Shop - hidden from tab bar, but keeps tab bar visible */}
+      <Tabs.Screen
+        name="shop/[category]"
+        options={{
+          href: null,
+        }}
+      />
     </Tabs>
   );
 }
